c22320810270040c45ace603b1377b405cd8385e
EugenieF <eug.francon@gmail.com>
2022-01-09 20:46:26 +0100

    to test on vm

--- philo_bonus/srcs/supervise_life_philo.c
+++ philo_bonus/srcs/supervise_life_philo.c
@@ -6,7 +6,7 @@
 /*   By: efrancon <efrancon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 13:30:52 by EugenieFr         #+#    #+#             */
-/*   Updated: 2022/01/09 19:30:26 by efrancon         ###   ########.fr       */
+/*   Updated: 2022/01/09 20:40:35 by efrancon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@ static t_bool	philo_died(t_philo *philo, t_data *data)
 	sem_wait(philo->meal_lock);
 	if (time_to_die < get_time() - philo->last_meal)
 	{
+		sem_post(philo->meal_lock);
 		ret = TRUE;
 		sem_wait(philo->end_lock);
 		philo->is_dead = TRUE;
 		philo->end = TRUE;
 		sem_post(philo->end_lock);
 		display_death(philo, data);
+		return (ret);
 	}
 	sem_post(philo->meal_lock);
 	return (ret);
